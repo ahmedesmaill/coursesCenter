21c110f100bcbc610418c92ae1f02afea680193d
ahmed esmai <ahmedesmailv55@gmail.com>
2024-12-18 05:36:08 +0200

    edit

--- Controllers/TraineAccountController.cs
+++ Controllers/TraineAccountController.cs
@@ -9,53 +9,53 @@ using Microsoft.AspNetCore.Mvc;
 namespace coursesCenter.Controllers
 {
     [Authorize(Roles = "Manager")]
-    public class TraineAccountController : Controller
+    public class TraineeAccountController : Controller
     {
         private readonly IDepartmentRepositorty DepartmentRepo;
-        private readonly ITraineRepository TraineRepo;
+        private readonly ITraineRepository TraineeRepo;
         private readonly UserManager<ApplicationUser> userManager;
         private readonly SignInManager<ApplicationUser> signInManager;
         private readonly RoleManager<IdentityRole<int>> roleManager;
-        public TraineAccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole<int>> roleManager, IDepartmentRepositorty DepartmentRepo, ITraineRepository TraineRepo)
+        public TraineeAccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole<int>> roleManager, IDepartmentRepositorty DepartmentRepo, ITraineRepository TraineeRepo)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
             this.roleManager = roleManager;
             this.DepartmentRepo= DepartmentRepo;
-            this.TraineRepo= TraineRepo;
+            this.TraineeRepo= TraineeRepo;
         }
         [HttpGet]
-        public IActionResult RegisterTraine()
+        public IActionResult RegisterTrainee()
         {
             ViewBag.department = DepartmentRepo.GetAll();
-            return View("RegisterTraine");
+            return View("RegisterTrainee");
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> RegisterTraine(RegisterTraineViewModel TraineRegiste)
+        public async Task<ActionResult> RegisterTrainee(RegisterTraineViewModel TraineeRegiste)
         {
             if(ModelState.IsValid)
             {
-                if (TraineRegiste.DepartmentId == 0)
+                if (TraineeRegiste.DepartmentId == 0)
                 {
                     ModelState.AddModelError("", "you should choose department");
                 }
                 else
                 {
                     ApplicationUser user = new ApplicationUser();
-                    user.Email = TraineRegiste.EmailAddress;
-                    user.UserName = TraineRegiste.Name;
-                    IdentityResult result = await userManager.CreateAsync(user, TraineRegiste.Password);
+                    user.Email = TraineeRegiste.EmailAddress;
+                    user.UserName = TraineeRegiste.Name;
+                    IdentityResult result = await userManager.CreateAsync(user, TraineeRegiste.Password);
                     if (result.Succeeded)
                     {
-                        Traine traine = new Traine();
-                        traine.Name = TraineRegiste.Name;
-                        traine.Address = TraineRegiste.Address;
-                        traine.Level = TraineRegiste.Level;
-                        traine.DepartmentId = TraineRegiste.DepartmentId;
-                        traine.ApplicationUserId = user.Id;
-                        TraineRepo.Save(traine);
-                        await userManager.AddToRoleAsync(user, "Traine");
+                        Traine trainee = new Traine();
+                        trainee.Name = TraineeRegiste.Name;
+                        trainee.Address = TraineeRegiste.Address;
+                        trainee.Level = TraineeRegiste.Level;
+                        trainee.DepartmentId = TraineeRegiste.DepartmentId;
+                        trainee.ApplicationUserId = user.Id;
+                        TraineeRepo.Save(trainee);
+                        await userManager.AddToRoleAsync(user, "Trainee");
                         await signInManager.SignInAsync(user, isPersistent: false);
                         return RedirectToAction("Index", "Home");
                     }
@@ -69,7 +69,7 @@ namespace coursesCenter.Controllers
                 }
             }
             ViewBag.department = DepartmentRepo.GetAll();
-            return View("RegisterTraine", TraineRegiste);
+            return View("RegisterTrainee", TraineeRegiste);
         }
     }
 }
